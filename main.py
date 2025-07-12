b1d608417707d074c9d403973d0e899c59a6d6b7
Katarina Duric <kd374@cornell.edu>
2025-07-12 01:34:18 -0400

    Katarina's comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello, this is Alejandro, your captain!\nPlease add a print with your name then pull/push the commit.")
     print("Hello, this is Ashley!\n I love pho.")
+    print("Hello, this is Katarina, and I am Ashley's no. 1 fan")
 
 if __name__ == "__main__":
     main()
