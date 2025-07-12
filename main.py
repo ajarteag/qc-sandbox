c85c87f7e49d2c792d91eef302558f65e0de1179
Kerry Huang <kerryhuang@Kerrys-MacBook-Pro.local>
2025-07-11 22:55:34 -0700

    kerry

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print("Hello, this is Alejandro, your captain!\nPlease add a print with your name then pull/push the commit.")
     print("Hello, this is Ashley!\n I love pho.")
     print("Hello, this is Katarina, and I am Ashley's no. 1 fan")
+    print("Hello, this is kerry")
 
 if __name__ == "__main__":
     main()
