c9c1eaecca2a5ce6272e9a4268605d8536802f73
Dan Arnold <danarnold38438@gmail.com>
2025-07-12 01:08:01 -0500

    initial message

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print("Hello, this is Ashley!\n I love pho.")
     print("Hello, this is Katarina, and I am Ashley's no. 1 fan")
     print("Hello, this is kerry")
+    print("Hello, this is Dan, your Vice-Captain")
 
 if __name__ == "__main__":
     main()
