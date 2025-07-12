fd9311eccb180952eec804d40ad283123174c9ea
AshleyYFu <yue.yang.fu@vanderbilt.edu>
2025-07-11 22:32:31 -0700

    Ashley's commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("Hello, this is Alejandro, your captain!\nPlease add a print with your name then pull/push the commit.")
+    print("Hello, this is Ashley!\n I love pho.")
 
 if __name__ == "__main__":
     main()
